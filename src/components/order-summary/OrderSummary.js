55027603bc32d0e8aa1502e9b39865adfded17aa
moniruzzaman01 <moniruzzamanshakib04@gmail.com>
2022-03-24 01:58:56 +0600

    cart calculation done

--- src/components/order-summary/OrderSummary.js
+++ src/components/order-summary/OrderSummary.js
@@ -2,17 +2,33 @@ import React from "react";
 import "./OrderSummary.css";
 
 function OrderSummary(props) {
-  console.log(props.cart[0]);
-  const add = (prev, current) => prev + current.price;
+  const { cart } = props;
+  // console.log(cart);
+  // const getQuantity = (prev, current) => prev + current.quantity;
+  // const quantity = cart.reduce(getQuantity, 0);
+  // const getTotalPrice = (prev, current) => prev + current.price;
+  // const getShippedPrice = (prev, current) => prev + current.shipping;
+  // const total = cart.reduce(getTotalPrice, 0);
+  // const shippingFee = cart.reduce(getShippedPrice, 0);
+  let total = 0;
+  let shippingFee = 0;
+  let quantity = 0;
+  for (const product of cart) {
+    quantity = quantity + product.quantity;
+    total = total + product.price * quantity;
+    shippingFee = shippingFee + product.shipping;
+  }
+  const tax = total * 0.1;
+  const grandTotal = total + shippingFee + tax;
   return (
     <div className="order-summary">
       <div className="order">
         <h2>Order Summary</h2>
-        <p>Added Product : {props.cart.length}</p>
-        <p>Total Price :{props.cart.reduce(add, 0)}</p>
-        <p>Total Shipping Charge : </p>
-        <p>Tax : </p>
-        <h3>Grand Total : </h3>
+        <p>Added Product : {quantity}</p>
+        <p>Total Price :{total}</p>
+        <p>Shipping Charge :{shippingFee} </p>
+        <p>Tax :{tax.toFixed(2)} </p>
+        <h3>Grand Total :{grandTotal.toFixed(2)} </h3>
       </div>
     </div>
   );
--- src/components/shop/Shop.js
+++ src/components/shop/Shop.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { addToLocalStorage, getStoredData } from "../../utilities/functions";
 import OrderSummary from "../order-summary/OrderSummary";
 import Product from "../product/Product";
 import "./Shop.css";
@@ -10,11 +11,37 @@ function Shop() {
       .then((res) => res.json())
       .then((data) => setProducts(data));
   }, []);
+
   const [cart, setCart] = useState([]);
   const btnClickHandle = (props) => {
+    //my process to handle first time quantity problem
+    console.log("prev", props.quantity);
+    if (props.quantity === 0) {
+      props.quantity = 1;
+    }
+    // console.log(props.quantity);
+    //--------------------------------
     const newCart = [...cart, props];
     setCart(newCart);
+    //add to local storage
+    addToLocalStorage(props.id);
   };
+  //get data from local storage and pass it to order summary
+  useEffect(() => {
+    const storedCart = getStoredData();
+    const ss = [];
+    for (const id in storedCart) {
+      const cartedProduct = products.find((product) => product.id === id);
+      if (cartedProduct) {
+        const quantity = storedCart[id];
+        cartedProduct.quantity = quantity;
+        // console.log("quantity", quantity);
+        ss.push(cartedProduct);
+      }
+    }
+    setCart(ss);
+  }, [products]);
+
   return (
     <div className="shop-container">
       <div className="products-container">
